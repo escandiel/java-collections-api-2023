be3a65f366fd52aee57eb379aa0aa27a4cf253e8
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-03 16:31:19 -0300

    new method

---
+++ src/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,14 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    //atributo
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+}

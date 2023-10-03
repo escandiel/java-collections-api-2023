3628b7e6d04f6383bcaf2181072e8b7778afcb74
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-03 16:29:43 -0300

    new method

---
+++ src/list/Ordenacao/Pessoa.java
@@ -0,0 +1,23 @@
+package list.Ordenacao;
+
+public class Pessoa {
+    //atributos
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
+}

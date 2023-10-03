c45312baa2c7f6f4cdb4780806830e1e66d7cb86
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-03 15:47:33 -0300

    creating Tarefa,ListaTarefa class and their methods

---
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,10 @@
+package list.OperacoesBasicas;
+
+public class Tarefa {
+    public Tarefa(String descricao) {
+    }
+
+    public String getDescricao() {
+        return getDescricao();
+    }
+}

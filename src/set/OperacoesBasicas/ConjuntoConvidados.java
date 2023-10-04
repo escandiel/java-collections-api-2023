9fbefaf05b34816f1cc3ebf2f824929b84bb7726
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-04 14:58:27 -0300

    creating the main args method

--- src/set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/set/OperacoesBasicas/ConjuntoConvidados.java
@@ -33,4 +33,17 @@ public class ConjuntoConvidados {
     public void exibirConvidados() {
         System.out.println(convidadoSet);
     }
+
+    public static void main(String[] args){
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println(" Existem" + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convdiados" );
+
+        conjuntoConvidados.adicionarConvidado("Convidado 1", 1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2", 1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3", 1236);
+        conjuntoConvidados.adicionarConvidado("Convidado 4", 1237);
+        conjuntoConvidados.adicionarConvidado("Convidado 5", 1238);
+        System.out.println(" Existem" + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convdiados" );
+
+    }
 }

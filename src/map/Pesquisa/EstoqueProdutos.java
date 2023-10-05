2028ed85ef5d9d609d832f9d9e1594db1f2e16ac
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-05 14:58:48 -0300

    new method: main

--- src/map/Pesquisa/EstoqueProdutos.java
+++ src/map/Pesquisa/EstoqueProdutos.java
@@ -42,4 +42,15 @@ public class EstoqueProdutos {
         }
         return produtoMaisCaro;
     }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+        estoque.exibirProdutos();
+        estoque.adicionarProduto(1l,"nike1", 120, 5.0);
+        estoque.adicionarProduto(2l,"nike2", 80, 8.0);
+        estoque.adicionarProduto(3l,"nike3", 10, 9.0);
+        estoque.exibirProdutos();
+        System.out.println("Valor total do estoque: R$ " + estoque.calcularValorTotalEstoque());
+        System.out.println("Produto mais caro: R$ " + estoque.obterProdutoMaisCaro());
+    }
 }

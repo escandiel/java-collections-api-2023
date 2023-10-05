85b9fbf64f1437d12075b28d12fb46a8e043c38a
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-05 14:42:01 -0300

    Constructor, Map and HashMap

--- src/map/Pesquisa/EstoqueProdutos.java
+++ src/map/Pesquisa/EstoqueProdutos.java
@@ -1,4 +1,13 @@
 package map.Pesquisa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EstoqueProdutos {
+    //atributo
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
 }

14b7fb3edd4fc6e312d04fbe2dc4ade3d4a7925e
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-05 14:13:52 -0300

    creating methods for the Class Agenda Contatos and importing Map and HashMap

--- src/map/OperacoesBasicas/AgendaContatos.java
+++ src/map/OperacoesBasicas/AgendaContatos.java
@@ -1,4 +1,13 @@
 package map.OperacoesBasicas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AgendaContatos {
+    //atributo
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
 }

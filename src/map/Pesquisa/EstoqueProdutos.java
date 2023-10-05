a78f9927525da4d3623d570042622e4e7ad48a1b
Victor Escandiel <joaoescandielb@gmail.com>
2023-10-05 14:48:48 -0300

    new methods: adicionarProdutos, exibirProdutos, calcularValorTotalEstoque

--- src/map/Pesquisa/EstoqueProdutos.java
+++ src/map/Pesquisa/EstoqueProdutos.java
@@ -10,4 +10,23 @@ public class EstoqueProdutos {
     public EstoqueProdutos() {
         this.estoqueProdutosMap = new HashMap<>();
     }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProdutos(){
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calcularValorTotalEstoque() {
+        double valorTotalEstoque = 0;
+        if(!estoqueProdutosMap.isEmpty()) {
+            for(Produto p : estoqueProdutosMap.values()) {
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+
+            }
+        }
+        return valorTotalEstoque;
+    }
 }
